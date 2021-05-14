e8fa5c29cdf10378c207a970065804441c311524
Janina-sh <janina.schindler@edu.fh-joanneum.at>
2021-05-14 23:31:54 +0200

    built for IOS

---
+++ BuildsIOS/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,87 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void MultipleImageTracking::Awake()
+extern void MultipleImageTracking_Awake_m1B463664181C019F73CA44F55A480035C7EB9E98 (void);
+// 0x00000002 System.Void MultipleImageTracking::OnEnable()
+extern void MultipleImageTracking_OnEnable_m61206B5D784D5A5594BFAE1AE4BC7828EAC53674 (void);
+// 0x00000003 System.Void MultipleImageTracking::OnDisable()
+extern void MultipleImageTracking_OnDisable_mFFEC01C405624C4965D5F8ED046091025BB377A8 (void);
+// 0x00000004 System.Void MultipleImageTracking::ImageChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void MultipleImageTracking_ImageChanged_m4CBD1BC7FCA62ACB6BF332D7C34CA388D9875E8C (void);
+// 0x00000005 System.Void MultipleImageTracking::UpdateImage(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void MultipleImageTracking_UpdateImage_mA149577917421743A152C69FEDB67065A9679CA1 (void);
+// 0x00000006 System.Void MultipleImageTracking::.ctor()
+extern void MultipleImageTracking__ctor_m4B0FB25979E7C6A6116A0D41B9BBA74CD62C44A8 (void);
+// 0x00000007 System.Void TestScript::OnEnable()
+extern void TestScript_OnEnable_mB17CF2DA626C374D9D21361537A6079C71958332 (void);
+// 0x00000008 System.Void TestScript::OnDisable()
+extern void TestScript_OnDisable_m888400BE7F10D0A6304B5EA1091068CA7B00126D (void);
+// 0x00000009 System.Void TestScript::OnChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void TestScript_OnChanged_mCCD4CB92CD6C0D40EDC36828109D5EA8CD9E3770 (void);
+// 0x0000000A System.Void TestScript::UpdateImage(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void TestScript_UpdateImage_m6BE6E660DAA77535C9EDC0058BCBD84FEDBD7D71 (void);
+// 0x0000000B System.Void TestScript::.ctor()
+extern void TestScript__ctor_m58A026B07FA116838E76CF62F4630A99FB96365C (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	MultipleImageTracking_Awake_m1B463664181C019F73CA44F55A480035C7EB9E98,
+	MultipleImageTracking_OnEnable_m61206B5D784D5A5594BFAE1AE4BC7828EAC53674,
+	MultipleImageTracking_OnDisable_mFFEC01C405624C4965D5F8ED046091025BB377A8,
+	MultipleImageTracking_ImageChanged_m4CBD1BC7FCA62ACB6BF332D7C34CA388D9875E8C,
+	MultipleImageTracking_UpdateImage_mA149577917421743A152C69FEDB67065A9679CA1,
+	MultipleImageTracking__ctor_m4B0FB25979E7C6A6116A0D41B9BBA74CD62C44A8,
+	TestScript_OnEnable_mB17CF2DA626C374D9D21361537A6079C71958332,
+	TestScript_OnDisable_m888400BE7F10D0A6304B5EA1091068CA7B00126D,
+	TestScript_OnChanged_mCCD4CB92CD6C0D40EDC36828109D5EA8CD9E3770,
+	TestScript_UpdateImage_m6BE6E660DAA77535C9EDC0058BCBD84FEDBD7D71,
+	TestScript__ctor_m58A026B07FA116838E76CF62F4630A99FB96365C,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1858,
+	1858,
+	1858,
+	1499,
+	1541,
+	1858,
+	1858,
+	1858,
+	1499,
+	1541,
+	1858,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
