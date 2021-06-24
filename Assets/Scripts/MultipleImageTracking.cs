88c769418244d7d77251656504f230e95edc2ae0
lotsa11 <lisafresser@gmx.de>
2021-06-24 09:42:20 +0200

    finalfuerpräsi

--- Assets/Scripts/MultipleImageTracking.cs
+++ Assets/Scripts/MultipleImageTracking.cs
@@ -7,27 +7,32 @@ using UnityEngine.XR.ARFoundation;
 
 
 [RequireComponent(typeof(ARTrackedImageManager))]
+[RequireComponent(typeof(AudioSource))]
 public class MultipleImageTracking : MonoBehaviour
 {
     [SerializeField] private GameObject[] placeablePrefabs; //our Prefabs, that will be created at runtime
     
     private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();  //will call sporned prefabs out of Prefab Array (string = used for finding a prefab from the placeable Prefab array with the same name
-    
+    public AudioSource geschichte;
     private ARTrackedImageManager trackedImageManager; //contains the reference image library, detects the images in it
 public bool showUICamera = false; //true = show UI camera, hide AR camera
 //public Camera ARcamera;
-
+private void Start(){
+geschichte = GetComponent<AudioSource>();
+}
     private void Awake()
     {
       //  ARcamera.enabled = false;
         trackedImageManager = FindObjectOfType<ARTrackedImageManager>(); //getting and storing a reference to the trackedImageManager
-        
+           Debug.Log("Prefab 1");
         //presporn one of the placeable Prefabs in our Array
         foreach (GameObject prefab in placeablePrefabs)
         {
+            Debug.Log("Geschichte  " + geschichte);
             GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
             newPrefab.name = prefab.name; //searching for the right prefab
             spawnedPrefabs.Add(prefab.name, newPrefab); //add this to the spawnedPrefab Dictionary, newPrefab=Reference
+           // geschichte.Play();
         }
     }
 
